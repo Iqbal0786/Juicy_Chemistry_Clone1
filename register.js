b1029573a4befb6ec761072dbf84525208d83c18
pavanpuli <pavanpuli9848@gmail.com>
2021-12-18 16:28:09 +0530

    register updated

--- register.js
+++ register.js
@@ -1,62 +1,19 @@
-function signup(e) {
-    e.preventDefault()
+document.querySelector("button").addEventListener("click", CONTINUE);
+var userData = JSON.parse(localStorage.getItem("userDatabase")) || [];
+function CONTINUE(event) {
+  event.preventDefault()
+  var email = document.querySelector("#email").value;
+  var password = document.querySelector("#password").value;
+  var user = document.querySelector("#firstname").value;
+  var mobile = document.querySelector("#mobile").value;
 
-    var register = document.getElementById("register");
-
-    var firstname = register.firstname.value;
-    var lastname = register.lastname.value;
-    var mob   = register.mob.value;
-    var email = register.email.value;
-    var password = register.password.value;
-
-
-register.firstname.value=null;
- 
-register.lastname.value=null;
- 
-register.mob.value=null;
-    
-register.email.value=null;
+  var userCred = {
+    emailAdd: email,
+    pass: password,
+    userName: user,
+    mobile: mobile,
+  };
+  userData.push(userCred);
+  localStorage.setItem("userDatabase", JSON.stringify(userData));
   
-register.password.value=null;
-
-
-    // should go to the local Storage
-    if(localStorage.getItem("users") === null) {
-        localStorage.setItem("users", JSON.stringify([]));
-    }
-
-    // get all the user data 
-    var users = JSON.parse(localStorage.getItem("users"))        // existing users
-
-    if (already_user(firstname, email, users)) {
-        alert("User Already Exists");
-    } else {
-        var user = {
-            firstname,
-            lastname,
-            mob,
-            email,
-            password,
-        };
-
-        // get array from localStorage
-
-
-        //push new user
-        users.push(user)
-
-        localStorage.setItem("users", JSON.stringify(users));
-        window.location.href = ""
-    }
 }
-//check if user already exits or not
-       function already_user(name,email,users){
-          for(let i = 0;i<users.length;i++) {
-               if(users[i].name === name && users[i].email === email){
-                 return true;
-               }
-           };
-           return false;
-
-       }
